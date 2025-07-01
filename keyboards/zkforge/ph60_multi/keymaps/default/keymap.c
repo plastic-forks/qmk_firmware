f5d86346d25d5aa4d6658ee190cc8cdce42c6617
Zeke Dou <zeke@zekedou.live>
2025-07-01 08:51:00 +0800

    feat(zkforge): disable the mouse key switcher for ph60_multi

--- keyboards/zkforge/ph60_multi/keymaps/default/keymap.c
+++ keyboards/zkforge/ph60_multi/keymaps/default/keymap.c
@@ -10,7 +10,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          MO(3),
-        MO(2),   KC_LGUI, KC_LALT,                            LT(4, KC_SPC),                               KC_RALT, KC_RGUI, KC_RCTL
+        MO(2),   KC_LGUI, KC_LALT,                            KC_SPC,                                      KC_RALT, KC_RGUI, KC_RCTL
     ),
 
     // BASE Layer - DVORAK
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH, KC_EQL,  KC_BSLS,
         KC_LCTL, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS,          KC_ENT,
         KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT,          MO(3),
-        MO(2),   KC_LGUI, KC_LALT,                            LT(4, KC_SPC),                               KC_RALT, KC_RGUI, KC_RCTL
+        MO(2),   KC_LGUI, KC_LALT,                            KC_SPC,                                      KC_RALT, KC_RGUI, KC_RCTL
     ),
 
     // Extended Layer
