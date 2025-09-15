1243fa729cfcb2aa337906fe4a3ddc60ded98605
Zeke Dou <zeke@zekedou.live>
2025-09-15 11:46:08 +0800

    feat(zkforge): add arrow layer to ph60_multi

--- keyboards/zkforge/ph60_multi/keymaps/default/keymap.c
+++ keyboards/zkforge/ph60_multi/keymaps/default/keymap.c
@@ -9,7 +9,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          MO(3),
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          MO(4),
         MO(2),   KC_LGUI, KC_LALT,                            KC_SPC,                                      KC_RALT, KC_RGUI, KC_RCTL
     ),
 
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_LBRC, KC_RBRC, KC_BSPC,
         KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH, KC_EQL,  KC_BSLS,
         KC_LCTL, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS,          KC_ENT,
-        KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT,          MO(3),
+        KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT,          MO(4),
         MO(2),   KC_LGUI, KC_LALT,                            KC_SPC,                                      KC_RALT, KC_RGUI, KC_RCTL
     ),
 
@@ -31,17 +31,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______,                            _______,                                     _______, _______, _______
     ),
 
-    // Function Layer
+    // Arrow Layer
     [3] = LAYOUT_60_ansi_tsangan_split_rshift(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_UP,            _______,
+        _______, _______, _______,                            _______,                                     KC_LEFT, KC_DOWN, KC_RGHT
+    ),
+
+
+    // Function Layer
+    [4] = LAYOUT_60_ansi_tsangan_split_rshift(
         _______, KC_BRID, KC_BRIU, KC_MCTL, KC_LPAD, _______, KC_SLEP, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, _______,
         _______, DF(0),   DF(1),   _______, QK_RBT,  _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
-        _______, _______, _______, _______, _______, QK_BOOT, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, _______, QK_BOOT, _______, _______, _______, _______, TG(3),   _______,          _______,
         _______, _______, _______,                            _______,                                     _______, _______, _______
     ),
 
     // Mouse Layer
-    [4] = LAYOUT_60_ansi_tsangan_split_rshift(
+    [5] = LAYOUT_60_ansi_tsangan_split_rshift(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, MS_ACL2, MS_ACL1, MS_ACL0, _______, MS_WHLU, MS_UP,   _______, _______, _______, _______, _______,
         _______, _______, _______, MS_BTN2, MS_BTN1, _______, _______, MS_LEFT, MS_DOWN, MS_RGHT, _______, _______,          _______,
@@ -50,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 };
 
-/* emulate ctrl+super+arrow without pressing MO(2) */
+/* emulate ctrl+super+arrow without using MO(2) or MO(3) */
 
 uint8_t filter_left_mods(uint8_t mod_state) {
     uint8_t left_mods = 0;
